e1d2ec298efce88547a4de238378dc01706b1814
veritius <veritiusgaming@gmail.com>
2023-11-06 17:22:46 +1100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+//! A drowning simulator.
+
+#![warn(missing_docs)]
+
+mod structure;
+
+fn main() {
+    println!("Hello, world!");
+}
---
+++ src/structure/mod.rs
@@ -0,0 +1,17 @@
+//! [Structure] entities and their child entities.
+
+use bevy::prelude::*;
+
+/// A structure entity.
+#[derive(Component)]
+pub struct Structure {
+    pub name: String,
+}
+
+/// A bundle of components, with everything you need to have a functional [Structure] entity.
+#[derive(Bundle)]
+pub struct StructureBundle {
+    pub structure: Structure,
+    pub transform: Transform,
+    pub visibility: Visibility,
+}
