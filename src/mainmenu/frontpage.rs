ccaa18666b1a54a09fe9d3217078f6e8c0e6c631
veritius <veritiusgaming@gmail.com>
2023-11-09 17:39:38 +1100

    Improved main menu UI layout

--- src/mainmenu/frontpage.rs
+++ src/mainmenu/frontpage.rs
@@ -1,5 +1,5 @@
 use bevy::prelude::*;
-use bevy_egui::{egui, EguiContexts};
+use bevy_egui::{egui::{self, Align2}, EguiContexts};
 use super::MainMenuPage;
 
 pub(super) fn front_page_system(
@@ -8,7 +8,12 @@ pub(super) fn front_page_system(
 ) {
     let ctx = contexts.ctx_mut();
 
-    egui::Window::new("Thalassophobia").show(ctx, |ui| {
+    egui::Window::new("Thalassophobia")
+    .anchor(Align2::CENTER_CENTER, egui::Vec2::splat(0.0))
+    .resizable(false)
+    .collapsible(false)
+    .movable(false)
+    .show(ctx, |ui| {
         if ui.button("Settings").clicked() {
             *page = MainMenuPage::Settings;
         }
