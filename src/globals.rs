d1ba188568cca320ea2b4c63e0a3fcbca6250c8d
veritius <veritiusgaming@gmail.com>
2023-11-09 13:51:58 +1100

    Better physics flags

--- src/globals.rs
+++ src/globals.rs
@@ -1,12 +1,16 @@
 //! Unchanging global variables, such as physics masks.
 
-/// Terrain, such as rock formations.
+/// Static or dynamic terrain segments.
 pub const PHYSICS_TERRAIN: u32 = 1 << 0;
-/// Structures, such as submarines, stations, and wrecks.
+
+/// Submarines, stations, and wrecks.
 pub const PHYSICS_STRUCTURE: u32 = 1 << 1;
-/// Creatures, such as humans or mobile alien wildlife.
+
+/// Humans or alien wildlife.
 pub const PHYSICS_CREATURE: u32 = 1 << 2;
-/// Bullets, including hitscan and projectile.
-pub const PHYSICS_BULLET: u32 = 1 << 3;
-/// Items, such as those dropped by players.
+
+/// A weapon hurtbox.
+pub const PHYSICS_WEAPON: u32 = 1 << 3;
+
+/// An item on the floor.
 pub const PHYSICS_ITEM: u32 = 1 << 4;
