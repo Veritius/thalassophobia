bc8eed883eae8736a402afda1740c757ebb57989
veritius <veritiusgaming@gmail.com>
2023-11-09 13:54:44 +1100

    Move physics consts into a module

--- src/globals.rs
+++ src/globals.rs
@@ -1,16 +1,19 @@
-//! Unchanging global variables, such as physics masks.
+//! Things that are global across the entire program, and decided at compile time.
 
-/// Static or dynamic terrain segments.
-pub const PHYSICS_TERRAIN: u32 = 1 << 0;
+/// Physics-related globals, like masks.
+pub mod physics {
+    /// Static or dynamic terrain segments.
+    pub const PHYSICS_TERRAIN: u32 = 1 << 0;
 
-/// Submarines, stations, and wrecks.
-pub const PHYSICS_STRUCTURE: u32 = 1 << 1;
+    /// Submarines, stations, and wrecks.
+    pub const PHYSICS_STRUCTURE: u32 = 1 << 1;
 
-/// Humans or alien wildlife.
-pub const PHYSICS_CREATURE: u32 = 1 << 2;
+    /// Humans or alien wildlife.
+    pub const PHYSICS_CREATURE: u32 = 1 << 2;
 
-/// A weapon hurtbox.
-pub const PHYSICS_WEAPON: u32 = 1 << 3;
+    /// A weapon hurtbox.
+    pub const PHYSICS_WEAPON: u32 = 1 << 3;
 
-/// An item on the floor.
-pub const PHYSICS_ITEM: u32 = 1 << 4;
+    /// An item on the floor.
+    pub const PHYSICS_ITEM: u32 = 1 << 4;
+}
