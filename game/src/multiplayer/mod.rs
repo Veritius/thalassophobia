a600ad0c2c2f2b72db1f7039777e004db909a533
veritius <veritiusgaming@gmail.com>
2024-03-28 12:18:44 +1100

    Multiplayer stuff

--- game/src/multiplayer/mod.rs
+++ game/src/multiplayer/mod.rs
@@ -1,5 +1,13 @@
 use bevy::prelude::*;
 use bevy_stardust::prelude::*;
+use bevy_stardust_udp::*;
+
+const APP_NET_VERSION: ApplicationNetworkVersion = ApplicationNetworkVersion {
+    ident: 0x3E92B4A1CBD963DD,
+    major: 0,
+    minor: 0,
+    banlist: &[],
+};
 
 #[derive(Debug, Default, Clone, Copy, PartialEq, Eq, Hash, Reflect, States)]
 pub enum MultiplayerState {
@@ -12,6 +20,7 @@ pub enum MultiplayerState {
 pub(super) fn setup_multiplayer(app: &mut App) {
     app.add_plugins((
         StardustPlugin,
+        UdpTransportPlugin::balanced(APP_NET_VERSION),
     ));
 
     app.register_type::<MultiplayerState>();
