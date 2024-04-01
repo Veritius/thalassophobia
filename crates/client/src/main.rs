9c2612856b4c4073a5cadb66af275e94542c83cc
veritius <veritiusgaming@gmail.com>
2024-04-01 18:10:20 +1100

    no main menu

--- crates/client/src/main.rs
+++ crates/client/src/main.rs
@@ -1,4 +1,3 @@
-mod mainmenu;
 mod settings;
 mod state;
 
@@ -8,7 +7,6 @@ fn main() {
     let mut app = App::new();
     shared::setup(&mut app);
 
-    app.add_plugins(mainmenu::MainMenuPlugin);
     app.add_plugins(state::GameStatePlugin);
 
     app.run();
