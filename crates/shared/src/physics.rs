20455bd128432beb2e770c1a68f09d1fc6795019
veritius <veritiusgaming@gmail.com>
2024-04-07 20:52:23 +1000

    a

--- crates/shared/src/physics.rs
+++ crates/shared/src/physics.rs
@@ -1,5 +1,5 @@
-use bevy_rapier3d::geometry::Group;
+use crate::rapier::geometry::Group;
 
-pub const PHYS_GROUP_TERRAIN:    Group = Group::GROUP_1;
+pub const PHYS_GROUP_TERRAIN:   Group = Group::GROUP_1;
 pub const PHYS_GROUP_STRUCTURE: Group = Group::GROUP_2;
 pub const PHYS_GROUP_CHARACTER: Group = Group::GROUP_3;
