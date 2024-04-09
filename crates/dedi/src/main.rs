e33caeacf2cb25218790b22b9d84fb372bf281fd
veritius <veritiusgaming@gmail.com>
2024-04-09 15:23:49 +1000

    stuff

--- crates/dedi/src/main.rs
+++ crates/dedi/src/main.rs
@@ -1,8 +1,17 @@
-use shared::bevy::prelude::*;
+use shared::{bevy::prelude::*, SetupMode};
 
 fn main() {
+    // Create app and add shared setup
     let mut app = App::new();
+    app.insert_resource(SetupMode::Headless);
     shared::pre_setup(&mut app);
 
+    // Client subsystems
+    /*
+        Nothing yet.
+    */
+
+    // Final shared setup and run app
+    shared::post_setup(&mut app);
     app.run();
 }
