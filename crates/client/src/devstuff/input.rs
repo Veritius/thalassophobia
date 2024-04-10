7c659eb86a351b2865b1df542507184d8d645c85
veritius <veritiusgaming@gmail.com>
2024-04-10 18:08:23 +1000

    Save changes

---
+++ crates/client/src/devstuff/input.rs
@@ -0,0 +1,19 @@
+use shared::bevy::prelude::*;
+use super::*;
+
+pub(super) fn show_hide_toggles_system(
+    keyboard: Res<ButtonInput<KeyCode>>,
+    mut inspector: ResMut<WorldInspectorVisibility>,
+    mut controllers: ResMut<PlayerControllerGizmos>,
+) {
+    if keyboard.just_pressed(KeyCode::F1) {
+        *inspector = match *inspector {
+            WorldInspectorVisibility::Show => WorldInspectorVisibility::Hide,
+            WorldInspectorVisibility::Hide => WorldInspectorVisibility::Show,
+        }
+    }
+
+    if keyboard.just_pressed(KeyCode::F3) {
+        controllers.config_window = !controllers.config_window;
+    }
+}
--- crates/client/src/devstuff/mod.rs
+++ crates/client/src/devstuff/mod.rs
@@ -1,13 +1,37 @@
+mod controllers;
+mod input;
+
 use bevy_egui::EguiPlugin;
-use shared::bevy::prelude::*;
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
+use shared::{bevy::prelude::*, bevy_ecs, bevy_reflect};
+use controllers::*;
 
 pub(crate) struct DevStuffPlugin;
 
 impl Plugin for DevStuffPlugin {
     fn build(&self, app: &mut App) {
-        // Add egui.
+        // Add bevy-inspector-egui
+        app.add_plugins(WorldInspectorPlugin::new()
+            .run_if(resource_exists_and_equals(WorldInspectorVisibility::Show)));
+
+        // Add egui (if it isn't added already)
         if !app.is_plugin_added::<EguiPlugin>() {
             app.add_plugins(EguiPlugin);
         }
+
+        app.add_systems(Update, input::show_hide_toggles_system);
+        app.add_systems(Update, (controllers::config_window, controllers::draw_gizmos));
+
+        app.register_type::<WorldInspectorVisibility>();
+        app.init_resource::<WorldInspectorVisibility>();
+        app.register_type::<PlayerControllerGizmos>();
+        app.init_resource::<PlayerControllerGizmos>();
     }
+}
+
+#[derive(Resource, Default, Reflect, PartialEq, Eq)]
+enum WorldInspectorVisibility {
+    Show,
+    #[default]
+    Hide,
 }
