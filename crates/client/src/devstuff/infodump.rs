1ddb94fecccde219c18f325ec11fc9fe401ad2d2
veritius <veritiusgaming@gmail.com>
2024-04-12 00:11:00 +1000

    Move gamestate into a panel in statistics window

--- crates/client/src/devstuff/infodump.rs
+++ crates/client/src/devstuff/infodump.rs
@@ -12,14 +12,30 @@ pub(super) enum InfodumpWindowVisibility {
 
 pub(super) fn infodump_window(
     mut ctx: EguiContexts,
+    visibility: Res<InfodumpWindowVisibility>,
+
     shared_state: Res<State<GameState>>,
     client_state: Res<State<ClientState>>,
-    visibility: Res<InfodumpWindowVisibility>,
+
     diagnostics: Res<DiagnosticsStore>,
 ) {
     if *visibility == InfodumpWindowVisibility::Hide { return; }
-    egui::Window::new("Information Dump").show(ctx.ctx_mut(), |ui| {
-        ui.label(format!("State: {} / {}", client_state.get(), shared_state.get()));
+    egui::Window::new("Statistics").show(ctx.ctx_mut(), |ui| {
+        // Game state panel
+        egui::CollapsingHeader::new("State")
+        .show(ui, |ui| {
+            egui::Grid::new("infodump_diagnostics")
+            .striped(true)
+            .show(ui, |ui| {
+                ui.label("Client state");
+                ui.label(format!("{}", client_state.get()));
+                ui.end_row();
+
+                ui.label("Shared state");
+                ui.label(format!("{}", shared_state.get()));
+                ui.end_row();
+            });
+        });
 
         // Diagnostics panel
         egui::CollapsingHeader::new("Diagnostics")
@@ -31,14 +47,14 @@ pub(super) fn infodump_window(
                 .show(ui, |ui| {
                     let count = diagnostics.iter().count();
                     let mut vec = Vec::with_capacity(count);
-    
+
                     for diagnostic in diagnostics.iter() {
                         vec.push((diagnostic.path(), diagnostic.value()));
                     }
-    
+
                     // Make the set ordered instead of weird and unstable
                     vec.sort_unstable_by(|(a,_),(b,_)| a.as_str().cmp(b.as_str()));
-    
+
                     for (name, value) in vec.drain(..) {
                         ui.label(name.as_str());
                         if let Some(value) = value {
