4744d236b2c168177f594f58ee5d4dcb90e357de
veritius <veritiusgaming@gmail.com>
2024-04-11 23:42:30 +1000

    Ordered diagnostics

--- crates/client/src/devstuff/infodump.rs
+++ crates/client/src/devstuff/infodump.rs
@@ -24,9 +24,19 @@ pub(super) fn infodump_window(
         // Diagnostics panel
         egui::CollapsingHeader::new("Diagnostics").show(ui, |ui| {
             egui::Grid::new("infodump_diagnostics").show(ui, |ui| {
+                let count = diagnostics.iter().count();
+                let mut vec = Vec::with_capacity(count);
+
                 for diagnostic in diagnostics.iter() {
-                    ui.label(diagnostic.path().as_str());
-                    if let Some(value) = diagnostic.value() {
+                    vec.push((diagnostic.path(), diagnostic.value()));
+                }
+
+                // Make the set ordered instead of weird and unstable
+                vec.sort_unstable_by(|(a,_),(b,_)| a.as_str().cmp(b.as_str()));
+
+                for (name, value) in vec.drain(..) {
+                    ui.label(name.as_str());
+                    if let Some(value) = value {
                         ui.label(format!("{value:.3}"));
                     }
                     ui.end_row();
