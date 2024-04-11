8a6dfe716535d42ce43f5bf6cef4096995f0ff35
veritius <veritiusgaming@gmail.com>
2024-04-11 23:47:35 +1000

    Put diagnostics in scroll area

--- crates/client/src/devstuff/infodump.rs
+++ crates/client/src/devstuff/infodump.rs
@@ -23,24 +23,26 @@ pub(super) fn infodump_window(
 
         // Diagnostics panel
         egui::CollapsingHeader::new("Diagnostics").show(ui, |ui| {
-            egui::Grid::new("infodump_diagnostics").show(ui, |ui| {
-                let count = diagnostics.iter().count();
-                let mut vec = Vec::with_capacity(count);
-
-                for diagnostic in diagnostics.iter() {
-                    vec.push((diagnostic.path(), diagnostic.value()));
-                }
-
-                // Make the set ordered instead of weird and unstable
-                vec.sort_unstable_by(|(a,_),(b,_)| a.as_str().cmp(b.as_str()));
-
-                for (name, value) in vec.drain(..) {
-                    ui.label(name.as_str());
-                    if let Some(value) = value {
-                        ui.label(format!("{value:.3}"));
+            egui::ScrollArea::vertical().show(ui, |ui| {
+                egui::Grid::new("infodump_diagnostics").show(ui, |ui| {
+                    let count = diagnostics.iter().count();
+                    let mut vec = Vec::with_capacity(count);
+    
+                    for diagnostic in diagnostics.iter() {
+                        vec.push((diagnostic.path(), diagnostic.value()));
                     }
-                    ui.end_row();
-                }
+    
+                    // Make the set ordered instead of weird and unstable
+                    vec.sort_unstable_by(|(a,_),(b,_)| a.as_str().cmp(b.as_str()));
+    
+                    for (name, value) in vec.drain(..) {
+                        ui.label(name.as_str());
+                        if let Some(value) = value {
+                            ui.label(format!("{value:.3}"));
+                        }
+                        ui.end_row();
+                    }
+                });
             });
         });
     });
