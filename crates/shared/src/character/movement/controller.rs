c8ab232a57c2d8431cee62a37f96f8d9d806dfd1
veritius <veritiusgaming@gmail.com>
2024-08-07 10:45:10 +1000

    systems

--- crates/shared/src/character/movement/controller.rs
+++ crates/shared/src/character/movement/controller.rs
@@ -1,4 +1,5 @@
-use crate::{bevy::prelude::*, math::transform::TranslateSet};
+use bevy_rapier3d::{plugin::RapierContext, prelude::*};
+use crate::{bevy::prelude::*, disabling::Disabled, math::transform::TranslateSet, physics::*};
 
 /// A player character controller.
 #[derive(Debug, Component, Reflect)]
@@ -32,4 +33,44 @@ pub struct PlayerController {
 pub struct PlayerControllerHead {
     /// Current rotation (pitch).
     pub rotation_pitch: f32,
+}
+
+pub(super) fn controller_grounding_system(
+    rapier_context: Res<RapierContext>,
+    // We can't filter out entities that haven't changed positions, since we can't account for other entities changing position.
+    mut controllers: Query<(Entity, &mut PlayerController, &GlobalTransform, Option<&CollisionGroups>), Without<Disabled>>,
+) {
+    for (entity, mut controller, transform, groups) in controllers.iter_mut() {
+        // Get membership data from a component if present, otherwise use a default
+        let group = if let Some(groups) = groups {
+            groups.clone()
+        } else {
+            // Default collision group
+            CollisionGroups {
+                memberships: PHYS_GROUP_CHARACTER,
+                filters: PHYS_GROUP_TERRAIN | PHYS_GROUP_STRUCTURE
+            }
+        };
+
+        // Cast a ray to see if there's anything below us to jump off of
+        let raycast = rapier_context.cast_ray(
+            transform.translation(),
+            Vec3::NEG_Y,
+            controller.ground_raycast_len,
+            false,
+            QueryFilter::default().exclude_collider(entity).groups(group),
+        );
+
+        // Apply the raycast result to the controller
+        match raycast {
+            Some(_) => { controller.is_touching_ground = true; },
+            None => { controller.is_touching_ground = false; },
+        }
+    }
+}
+
+pub(super) fn controller_movement_system(
+
+) {
+
 }
