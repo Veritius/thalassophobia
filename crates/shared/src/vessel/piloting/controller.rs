978b60a7d327a32da0adff5459890ec3ee3e8491
veritius <veritiusgaming@gmail.com>
2024-08-09 09:26:50 +1000

    Remove vessel move style

--- crates/shared/src/vessel/piloting/controller.rs
+++ crates/shared/src/vessel/piloting/controller.rs
@@ -4,23 +4,9 @@ use leafwing_input_manager::prelude::*;
 use crate::{disabling::Disabled, math::transform::TranslateSet};
 use super::VesselMovements;
 
-/// The 'style' of movement for vessels.
-#[derive(Default, PartialEq, Eq, Hash, Clone, Copy, Debug, Reflect)]
-pub enum VesselMoveStyle {
-    /// The vessel will not attempt to correct any movements.
-    #[default]
-    Manual,
-
-    /// The vessel will maintain speed and course.
-    Maintain,
-}
-
 /// A controller for a submarine.
 #[derive(Debug, Component, Reflect)]
 pub struct VesselController {
-    /// The current movement style of the vessel.
-    pub move_style: VesselMoveStyle,
-
     /// The maximum amount of force that can be applied to translate the vessel.
     pub translate_force: TranslateSet<f32>,
 
@@ -30,26 +16,18 @@ pub struct VesselController {
 
 pub(super) fn vessel_controller_system(
     mut bodies: Query<(
-        &mut VesselController,
+        &VesselController,
         &Transform,
         &mut ExternalImpulse,
         &ActionState<VesselMovements>,
     ), Without<Disabled>>,
 ) {
     for (
-        mut controller,
+        controller,
         transform,
         mut impulse,
         actions
     ) in bodies.iter_mut() {
-        // Let the person change the control style ahead of time
-        if actions.pressed(&VesselMovements::ChangeStyle) {
-            controller.move_style = match controller.move_style {
-                VesselMoveStyle::Manual => VesselMoveStyle::Maintain,
-                VesselMoveStyle::Maintain => VesselMoveStyle::Manual,
-            }
-        }
-
         // Translation intent value
         let mut translate_intent = Vec3::ZERO;
 
