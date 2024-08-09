b5bdb7481d13dca4797c019dc8ad90500d8d64e6
veritius <veritiusgaming@gmail.com>
2024-08-09 15:12:01 +1000

    phyisics stuff

--- crates/shared/src/physics.rs
+++ crates/shared/src/physics.rs
@@ -1,14 +1,16 @@
 pub use crate::avian::prelude::*;
 
-pub const PHYS_LAYER_TERRAIN:   LayerMask = LayerMask(1);
-pub const PHYS_LAYER_STRUCTURE: LayerMask = LayerMask(2);
-pub const PHYS_LAYER_CHARACTER: LayerMask = LayerMask(4);
+pub const PHYS_LAYER_TERRAIN   : LayerMask = LayerMask(1);
+pub const PHYS_LAYER_STRUCTURE : LayerMask = LayerMask(2);
+pub const PHYS_LAYER_VESSEL    : LayerMask = LayerMask(4);
+pub const PHYS_LAYER_CHARACTER : LayerMask = LayerMask(8);
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(i8)]
 pub enum DominancePreset {
     Terrain = 126,
-    Structures = 110,
+    Structure = 110,
+    Vessel = 94,
 }
 
 impl From<DominancePreset> for Dominance {
