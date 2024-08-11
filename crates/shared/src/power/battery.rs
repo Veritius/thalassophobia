def4d28ad41cab11a3b5f66c947689537a773d3d
veritius <veritiusgaming@gmail.com>
2024-08-12 09:32:35 +1000

    efficiency field in battery

--- crates/shared/src/power/battery.rs
+++ crates/shared/src/power/battery.rs
@@ -2,7 +2,7 @@ use crate::prelude::*;
 use super::Joule;
 
 /// A device that stores electricity.
-#[derive(Debug, Default, Clone, Component, Reflect, Serialize, Deserialize)]
+#[derive(Debug, Clone, Component, Reflect, Serialize, Deserialize)]
 #[reflect(Component, Serialize, Deserialize)]
 pub struct Battery {
     /// The amount of currently stored energy.
@@ -16,4 +16,20 @@ pub struct Battery {
 
     /// The speed at which electricity can be discharged.
     pub discharge: u32,
+
+    /// The efficiency of recharging the battery.
+    /// Ranges from `0.0` (0% efficient) to `1.0` (100% efficient).
+    pub efficiency: f32,
+}
+
+impl Default for Battery {
+    fn default() -> Self {
+        Self {
+            stored: Joule::from(0),
+            capacity: Joule::from(0),
+            recharge: 1,
+            discharge: 1,
+            efficiency: 1.0,
+        }
+    }
 }
