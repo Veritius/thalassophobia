e7e07291a6a41cbece5813d68d9eb5ef4e4f2ddc
veritius <veritiusgaming@gmail.com>
2024-08-12 13:03:55 +1000

    dominance and layer in prelude

--- crates/shared/src/prelude.rs
+++ crates/shared/src/prelude.rs
@@ -1,5 +1,6 @@
 pub use crate::{bevy::prelude::*, bevy_ecs, bevy_reflect, bevy_state};
 pub use crate::avian::prelude::*;
+pub use crate::physics::{ObjectLayer, ObjectDominance};
 pub use crate::disabling::Disabled;
 pub use crate::input::{Actionlike, input_map::InputMap, action_state::ActionState};
 pub use crate::schedules::SimulationUpdate;
