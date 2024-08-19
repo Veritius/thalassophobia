5ba185e4e873d47735bb1db00fa57995d696b143
veritius <veritiusgaming@gmail.com>
2024-08-19 16:43:22 +1000

    procrastination

--- crates/shared/src/vessel/piloting/controller.rs
+++ crates/shared/src/vessel/piloting/controller.rs
@@ -54,7 +54,8 @@ pub(super) fn vessel_controller_system(
         let mut rotation_intent = Vec3::ZERO;
 
         // Rotation inputs
-        rotation_intent.x += actions.clamped_value(&VesselMovements::Pitch);
+        // TODO: FIX THIS
+        // rotation_intent.x += actions.clamped_value(&VesselMovements::Pitch);
         rotation_intent.y -= actions.clamped_value(&VesselMovements::Yaw);
         rotation_intent.z += actions.clamped_value(&VesselMovements::Roll);
 
