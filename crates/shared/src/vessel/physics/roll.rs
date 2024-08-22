be0b53f20337dce3b368fd037a2f52a90e06b35f
veritius <veritiusgaming@gmail.com>
2024-08-22 14:52:09 +1000

    save

--- crates/shared/src/vessel/physics/roll.rs
+++ crates/shared/src/vessel/physics/roll.rs
@@ -1,5 +1,5 @@
 use std::{marker::PhantomData, ops::RangeInclusive};
-use crate::{math::{curve::FloatCurve, transform::Axis}, prelude::*};
+use crate::{math::{curve::FloatCurve, transform::{Axis, X, Y, Z}}, prelude::*};
 
 /// Imposes limits to the orientation of a vessel around an axis.
 /// The component is applied only to local space.
@@ -21,6 +21,66 @@ pub struct VesselAngleLimit<R: Axis> {
     pub reset: Option<RangeInclusive<f32>>,
 
     _phantom: PhantomData<R>,
+}
+
+pub(super) fn vessel_limit_system(
+    mut vessels: Query<(
+        Option<&VesselAngleLimit<X>>,
+        Option<&VesselAngleLimit<Y>>,
+        Option<&VesselAngleLimit<Z>>,
+
+        &Mass,
+        &mut Transform,
+        &mut ExternalAngularImpulse,
+    ), Or<(
+        With<VesselAngleLimit<X>>,
+        With<VesselAngleLimit<Y>>,
+        With<VesselAngleLimit<Z>>,
+    )>>,
+) {
+    for (
+        lm_x,
+        lm_y,
+        lm_z,
+
+        mass,
+        mut transform,
+        mut torque,
+    ) in &mut vessels {
+        fn angle_limit<R: Axis>(
+            current: f32,
+            limit: Option<&VesselAngleLimit<R>>,
+            mass: &Mass,
+            mut transform: Mut<Transform>,
+            mut torque: Mut<ExternalAngularImpulse>,
+        ) {
+            // If there is no limit, we don't care
+            if limit.is_none() { return }
+            let limit = limit.unwrap();
+
+            'reset: { if let Some(range) = &limit.reset {
+                // If the current angle is within range, don't do anything
+                if range.contains(&current) { break 'reset; }
+
+                todo!()
+            } }
+
+            'limit: { if let Some(range) = &limit.limit {
+                // If the current angle is within range, don't do anything
+                if range.contains(&current) { break 'limit; }
+
+                todo!()
+            } }
+        }
+
+        // Get the orientation of the entity in all three axes
+        let (yaw, pitch, roll) = transform.rotation.to_euler(EulerRot::YXZ);
+
+        // Apply to all three axes
+        angle_limit(pitch, lm_x, mass, transform.reborrow(), torque.reborrow());
+        angle_limit(yaw,   lm_y, mass, transform.reborrow(), torque.reborrow());
+        angle_limit(roll,  lm_z, mass, transform.reborrow(), torque.reborrow());
+    }
 }
 
 /// Applies a force to counteract roll in a vessel.
