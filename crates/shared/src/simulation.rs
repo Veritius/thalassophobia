be2da5e4992806ded3cffb5357d1a632b14b49d9
veritius <veritiusgaming@gmail.com>
2024-08-23 09:48:47 +1000

    a

--- crates/shared/src/simulation.rs
+++ crates/shared/src/simulation.rs
@@ -7,8 +7,6 @@ pub trait SimulationAppExt {
 
 impl SimulationAppExt for App {
     fn add_sim_event<E: Event>(&mut self) {
-        self.init_resource::<Events<E>>();
-
         self.add_systems(SimulationInit, |mut commands: Commands| commands.init_resource::<Events<E>>());
         self.add_systems(SimulationUpdate, |mut events: ResMut<Events<E>>| events.update());
         self.add_systems(SimulationClear, |mut commands: Commands| commands.remove_resource::<Events<E>>());
