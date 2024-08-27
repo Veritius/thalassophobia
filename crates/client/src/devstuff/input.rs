7278e129107cac354ff5acf5bed65c20e2f17e21
veritius <veritiusgaming@gmail.com>
2024-08-27 11:58:36 +1000

    swap inspector/infodump buttons

--- crates/client/src/devstuff/input.rs
+++ crates/client/src/devstuff/input.rs
@@ -3,20 +3,20 @@ use super::*;
 
 pub(super) fn show_hide_toggles_system(
     keyboard: Res<ButtonInput<KeyCode>>,
-    mut inspector: ResMut<WorldInspectorVisibility>,
     mut infodump: ResMut<InfodumpWindowVisibility>,
+    mut inspector: ResMut<WorldInspectorVisibility>,
 ) {
     if keyboard.just_pressed(KeyCode::F1) {
-        *inspector = match *inspector {
-            WorldInspectorVisibility::Show => WorldInspectorVisibility::Hide,
-            WorldInspectorVisibility::Hide => WorldInspectorVisibility::Show,
+        *infodump = match *infodump {
+            InfodumpWindowVisibility::Show => InfodumpWindowVisibility::Hide,
+            InfodumpWindowVisibility::Hide => InfodumpWindowVisibility::Show,
         }
     }
 
     if keyboard.just_pressed(KeyCode::F2) {
-        *infodump = match *infodump {
-            InfodumpWindowVisibility::Show => InfodumpWindowVisibility::Hide,
-            InfodumpWindowVisibility::Hide => InfodumpWindowVisibility::Show,
+        *inspector = match *inspector {
+            WorldInspectorVisibility::Show => WorldInspectorVisibility::Hide,
+            WorldInspectorVisibility::Hide => WorldInspectorVisibility::Show,
         }
     }
 }
