3009a7745bb45c7daee231649cbed11ac6c27bea
veritius <veritiusgaming@gmail.com>
2024-08-27 11:35:03 +1000

    Make server state a substate of initialisation

--- crates/server/src/lib.rs
+++ crates/server/src/lib.rs
@@ -9,14 +9,15 @@ pub struct LobbyHostingPlugin;
 impl Plugin for LobbyHostingPlugin {
     fn build(&self, app: &mut App) {
         app.register_type::<ServerState>();
-        app.init_state::<ServerState>();
+        app.add_sub_state::<ServerState>();
 
         app.register_type::<LobbySettings>();
     }
 }
 
 /// Whether or not the lobby is open.
-#[derive(Debug, Default, Clone, Copy, PartialEq, Eq, Hash, Reflect, States)]
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq, Hash, Reflect, SubStates)]
+#[source(Initialisation = Initialisation::Finished)]
 pub enum ServerState {
     /// No lobby is being hosted.
     #[default]
--- crates/shared/src/prelude.rs
+++ crates/shared/src/prelude.rs
@@ -1,14 +1,15 @@
 pub use crate::{bevy::prelude::*, bevy_ecs, bevy_reflect, bevy_state};
 pub use crate::avian::prelude::*;
-pub use crate::physics::{ObjectLayer, ObjectDominance};
 pub use crate::disabling::Disabled;
+pub use crate::initial::Initialisation;
 pub use crate::input::{Actionlike, input_map::InputMap, action_state::ActionState};
-pub use crate::simulation::{SimulationLoaded, SimulationRunning, SimulationUpdate};
 pub use crate::math::units::*;
-pub use crate::smallvec::{smallvec, SmallVec};
-pub use crate::setup::SetupMode;
-pub use crate::simulation::SimulationAppExt;
+pub use crate::physics::{ObjectLayer, ObjectDominance};
 pub use crate::progress::{Progress, Done};
+pub use crate::setup::SetupMode;
+pub use crate::simulation::{SimulationLoaded, SimulationRunning, SimulationUpdate};
+pub use crate::simulation::SimulationAppExt;
+pub use crate::smallvec::{smallvec, SmallVec};
 pub use serde::{Serialize, Deserialize};
 
 #[cfg(feature="multiplayer")]
