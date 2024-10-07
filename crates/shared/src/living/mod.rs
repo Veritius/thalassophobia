b7f704aabf1ac7b3fb1bb72fed6335717f4ba384
veritius <veritiusgaming@gmail.com>
2024-10-07 17:20:24 +1100

    save

--- crates/shared/src/living/mod.rs
+++ crates/shared/src/living/mod.rs
@@ -1,1 +1,1 @@
-pub mod balance;
+pub mod stuns;
---
+++ crates/shared/src/living/stuns/ragdoll.rs
@@ -0,0 +1,20 @@
+use crate::prelude::*;
+
+#[derive(Debug, Clone, Copy, Component, Reflect, Serialize, Deserialize)]
+#[reflect(Component, Serialize, Deserialize)]
+pub struct RagdollStun {
+    #[reflect(@0.0 ..= f32::INFINITY)]
+    pub seconds: f32,
+
+    pub resilience: f32,
+}
+
+impl Default for RagdollStun {
+    fn default() -> Self {
+        Self {
+            seconds: 0.0,
+
+            resilience: 1.0,
+        }
+    }
+}
