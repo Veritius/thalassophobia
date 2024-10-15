b8907030b1b1779422e2d9f65b25d45c8baa79cf
veritius <veritiusgaming@gmail.com>
2024-10-15 14:48:45 +1100

    save

--- crates/shared/src/math/curve.rs
+++ crates/shared/src/math/curve.rs
@@ -5,7 +5,7 @@ type PointSet = SmallVec<[Vec2; 2]>;
 #[derive(Debug, Clone, Reflect, Serialize, Deserialize)]
 #[reflect(Serialize, Deserialize)]
 pub enum FloatCurve {
-    Flat(f32),
+    Constant(f32),
     Logarithm(f32),
     Exponential(f32),
     LinearPoints(PointSet),
@@ -14,14 +14,14 @@ pub enum FloatCurve {
 
 impl Default for FloatCurve {
     fn default() -> Self {
-        Self::Flat(1.0)
+        Self::Constant(1.0)
     }
 }
 
 impl FloatCurve {
     pub fn sample(&self, at: f32) -> f32 {
         match self {
-            FloatCurve::Flat(v) => *v,
+            FloatCurve::Constant(v) => *v,
             FloatCurve::Logarithm(e) => at.log10() * e,
             FloatCurve::Exponential(e) => at.powf(*e),
             FloatCurve::LinearPoints(_) => todo!(),
