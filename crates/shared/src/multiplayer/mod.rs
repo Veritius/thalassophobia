2f91e83ff64ab634089dc98d691c5ad95059c59a
veritius <veritiusgaming@gmail.com>
2024-10-15 14:05:39 +1100

    Save

--- crates/shared/src/multiplayer/mod.rs
+++ crates/shared/src/multiplayer/mod.rs
@@ -8,5 +8,12 @@ pub(crate) struct MultiplayerPlugin;
 impl Plugin for MultiplayerPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugins(StardustPlugin);
+
+        app.register_relation::<Manages>();
     }
 }
+
+/// A relation from a [peer entity](crate::stardust::connections) to a [player](crate::players::Player).
+/// If a player is 'managed' it means it is controlled by a remote peer, and inputs originate from the peer.
+#[derive(Relation)]
+pub struct Manages;
