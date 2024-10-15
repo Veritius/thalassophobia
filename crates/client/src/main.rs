81ba7f58320b041de7be1de711fc81f2ce391abe
veritius <veritiusgaming@gmail.com>
2024-10-15 13:39:16 +1100

    save

--- crates/client/src/main.rs
+++ crates/client/src/main.rs
@@ -16,6 +16,10 @@ fn main() {
     app.add_plugins(DefaultPlugins);
     shared::setup(&mut app, SetupMode::Full);
 
+    // Dev stuff comes first
+    #[cfg(feature="devstuff")]
+    app.add_plugins(devstuff::DevStuffPlugin);
+
     // Server setup, if hosting is enabled
     #[cfg(feature="hosting")]
     app.add_plugins(server::LobbyHostingPlugin);
@@ -25,9 +29,6 @@ fn main() {
     app.add_plugins(initial::InitialisationUiPlugin);
     app.add_plugins(power::ElectricityPlugin);
     app.add_plugins(settings::UserSettingsPlugin);
-
-    #[cfg(feature="devstuff")]
-    app.add_plugins(devstuff::DevStuffPlugin);
 
     app.add_plugins(debugsys::DebugSystemsPlugin);
 
