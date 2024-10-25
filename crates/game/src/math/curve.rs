1ad775543c8ca3a2af63e702d6f630c9ea51d8fd
veritius <veritiusgaming@gmail.com>
2024-10-25 11:06:19 +1100

    save

--- crates/game/src/math/curve.rs
+++ crates/game/src/math/curve.rs
@@ -9,7 +9,7 @@ type PointSet = SmallVec<[Vec2; 2]>;
 pub enum FloatCurve {
     Constant(f32),
     LinearPoints(PointSet),
-    CubicPoints(PointSet),
+    SmoothedPoints(PointSet),
 }
 
 impl Default for FloatCurve {
@@ -26,18 +26,18 @@ impl FloatCurve {
 
             // You can't sample a lack of points
             FloatCurve::LinearPoints(p) |
-            FloatCurve::CubicPoints(p)
+            FloatCurve::SmoothedPoints(p)
                 if p.len() == 0 => f32::NAN,
 
             // One point is the same as Constant
             FloatCurve::LinearPoints(p) |
-            FloatCurve::CubicPoints(p)
+            FloatCurve::SmoothedPoints(p)
                 if p.len() == 1 => p[0].y,
 
             // Two points means linear interpolation
             FloatCurve::LinearPoints(p) |
-            FloatCurve::CubicPoints(p)
-                if p.len() == 2 => Self::lerp_pts(p, s),
+            FloatCurve::SmoothedPoints(p)
+                if p.len() == 2 => Self::lerp_pts_slice(p, s),
 
             // Linear points sample for any length above 2
             FloatCurve::LinearPoints(p) => {
@@ -46,16 +46,27 @@ impl FloatCurve {
                 // If less or greater than the bounds of the points in the set
                 // we have to extrapolate based on the two points on the start
                 // or end of the set, effectively continuing the line to infinity
-                if s < p[0].x { return Self::lerp_pts(&p[..2], s); }
-                if s > p[l-1].x { return Self::lerp_pts(&p[l-3..l-1], s); }
+                if s < p[0].x { return Self::lerp_pts_slice(&p[..2], s); }
+                if s > p[l-1].x { return Self::lerp_pts_slice(&p[l-3..l-1], s); }
 
-                Self::lerp_pts(
+                Self::lerp_pts_slice(
                     p.windows(2).find(|p| p[0].x <= s && p[1].x >= s).unwrap(),
                     s
                 )
             },
 
-            FloatCurve::CubicPoints(_) => todo!(),
+            FloatCurve::SmoothedPoints(p) => {
+                let l = p.len();
+
+                // out of bounds
+                if s < p[0].x { todo!() }
+                if s > p[l-1].x { todo!() }
+
+                Self::qerp_pts_slice(
+                    p.windows(3).find(|p| p[0].x <= s && p[1].x >= s).unwrap(),
+                    s
+                )
+            },
         }
     }
 
@@ -67,17 +78,49 @@ impl FloatCurve {
         Self::LinearPoints(PointSet::from_iter(iter.into_iter().map(|p| p.into())))
     }
 
-    pub fn cubic_points<I, P>(iter: I) -> Self
+    pub fn smoothed_points<I, P>(iter: I) -> Self
     where
         I: IntoIterator<Item = P>,
         P: Into<Vec2>,
     {
-        Self::CubicPoints(PointSet::from_iter(iter.into_iter().map(|p| p.into())))
+        Self::SmoothedPoints(PointSet::from_iter(iter.into_iter().map(|p| p.into())))
     }
 
-    fn lerp_pts(p: &[Vec2], s: f32) -> f32 {
-        let [[lx, ly], [rx, ry]] = [p[0].to_array(), p[1].to_array()];
-        return ly.lerp(ry, (s - lx) / (rx - lx));
+    fn lerp_pts(ax: f32, ay: f32, bx: f32, by: f32, s: f32) -> f32 {
+        ay.lerp(by, (s - ax) / (bx - ax))
+    }
+
+    fn lerp_pts_slice(p: &[Vec2], s: f32) -> f32 {
+        Self::lerp_pts(p[0].x, p[0].y, p[1].x, p[1].y, s)
+    }
+
+    fn qerp_pts(p: [f32; 6], s: f32) -> f32 {
+        // Points that we're working with
+        let [
+            ax, ay,
+            bx, by,
+            cx, cy,
+        ] = p;
+
+        // Intermediate pass of points
+        let [ka,kb] = [
+            Self::lerp_pts(ax, ay, bx, by, s),
+            Self::lerp_pts(bx, by, cx, cy, s),
+        ];
+
+        // Our final desired point
+        return ka.lerp(kb, s);
+    }
+
+    fn qerp_pts_slice(p: &[Vec2], s: f32) -> f32 {
+        Self::qerp_pts([
+            p[0].x,
+            p[0].y,
+            p[1].x,
+            p[1].y,
+            p[2].x,
+            p[2].y
+        ], s)
     }
 }
 
